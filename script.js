f43506338bb9da7644bbb7e642e9b1540a28142f
velibor918 <velibor918@gmail.com>
2024-04-08 13:44:58 +0200

    Added basic styling for rows

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let container = document.querySelector('.container')
 function createGrid(num){
     for(let i = 0; i < num; i++){
         const row = container.appendChild(document.createElement('div'));
+        row.classList.add('row')
     }
-        for(let j = 0; j < num; j++){
-            const square = document.createElement('div');
-            row.appendChild(square)
-        }
+
+    for(let j = 0; j < num; j++){
+        const square = document.createElement('div');
+        row.appendChild(square)
+    }
 }
 
 createGrid(16);
