d3111f5210420e9167291885ffc51d819948897f
velibor918 <velibor918@gmail.com>
2024-04-08 14:40:42 +0200

    Linked createGrid function to first button, renamed first button

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 
-let container = document.querySelector('.container')
+let container = document.querySelector('.container');
 
-function createGrid(num){
-    for(let i = 0; i < num; i++){
-        const row = container.appendChild(document.createElement('div'));
-        row.classList.add('row');
-    for(let j = 0; j < num; j++){
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.height = `${900 / row}px`
-        row.appendChild(square);
-        };
+let gridBtn = document.querySelector('.grid-creation');
+
+function createGrid(){
+    let num = Number(prompt('tasdgsdg'));
+    if (num > 100){
+        return alert("The grid can't be bigger than 100");   
     };
+        for(let i = 0; i < num; i++){
+            const row = container.appendChild(document.createElement('div'));
+            row.classList.add('row');
+        for(let j = 0; j < num; j++){
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.height = `${900 / row}px`;
+            row.appendChild(square);
+            };
+        };
 }
 
-createGrid(64);
+gridBtn.addEventListener('click', createGrid);
+
+// createGrid();
