51b275e3af50dceff8f8ef72c82f8225fa2a33c4
velibor918 <velibor918@gmail.com>
2024-04-08 13:40:47 +0200

    Added basic layout for grid function, will fix later

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let container = document.querySelector('.container')
+
+function createGrid(num){
+    for(let i = 0; i < num; i++){
+        const row = container.appendChild(document.createElement('div'));
+    }
+        for(let j = 0; j < num; j++){
+            const square = document.createElement('div');
+            row.appendChild(square)
+        }
+}
+
+createGrid(16);
