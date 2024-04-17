44d9142e19cb0ee65a854523bfcf31fea52aacce
velibor918 <velibor918@gmail.com>
2024-04-17 14:18:12 +0200

    Minor code formating

--- script.js
+++ script.js
@@ -3,80 +3,84 @@ let container = document.querySelector('.container');
 
 let gridBtn = document.querySelector('.grid-creation');
 
-function createGrid(){
-    while (container.firstChild){
+function createGrid() {
+    while (container.firstChild) {
         container.removeChild(container.lastChild)
     }
     let num = Number(prompt('Create a Grid.'));
-    if (num > 100){
-        return alert("The grid can't be bigger than 100.");   
+    if (num > 100) {
+        return alert("The grid can't be bigger than 100.");
     };
-        for(let i = 0; i < num; i++){
-            const row = container.appendChild(document.createElement('div'));
-            row.classList.add('row');
-        for(let j = 0; j < num; j++){
+    for (let i = 0; i < num; i++) {
+        const row = container.appendChild(document.createElement('div'));
+        row.classList.add('row');
+        for (let j = 0; j < num; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
             square.style.height = `${600 / row}px`;
             row.appendChild(square);
-            };
         };
+    };
 }
 
 gridBtn.addEventListener('click', createGrid);
 
 let colorChoice = document.querySelector('#color-picker');
 
-function changeColor () {
+function changeColor() {
     let children = document.querySelectorAll('.square');
 
     children.forEach((child) => {
         child.addEventListener('mouseenter', () => {
             child.style.backgroundColor = `${colorChoice.value}`;
         }
-     )}
-    )};
+        )
+    }
+    )
+};
 
 let colorBtn = document.querySelector('.change-color');
 
-colorBtn.addEventListener ('click', changeColor);
+colorBtn.addEventListener('click', changeColor);
 
 let eraseBtn = document.querySelector('.eraser');
 
-function erase () {
+function erase() {
     let children = document.querySelectorAll('.square');
 
     children.forEach((child) => {
         child.addEventListener('mouseenter', () => {
             child.style.backgroundColor = `white`;
         }
-     )}
-    )};
+        )
+    }
+    )
+};
 
 eraseBtn.addEventListener('click', erase);
 
 let clearBtn = document.querySelector('.clear-all');
 
-function clear () {
+function clear() {
     let children = document.querySelectorAll('.square');
 
     children.forEach((child) => {
         child.style.backgroundColor = `white`;
-    } );
+    });
 };
 
 clearBtn.addEventListener('click', clear);
 
 let randomBtn = document.querySelector('.random-color');
 
-function randomColor () {
+function randomColor() {
     let children = document.querySelectorAll('.square');
 
     children.forEach((child) => {
         child.addEventListener('mouseenter', () => {
-            child.style.backgroundColor = `rgb(${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)})`;
+            child.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
         })
-    } );
+    });
 };
 
 randomBtn.addEventListener('click', randomColor);
